24b6055842d24b85e1e247459eafa6730915fc75
Ayush Tiwari <ayushtiwari5747462@gmail.com>
2022-05-27 18:48:10 +0530

    Added block validation methods

--- blockchain/main.go
+++ blockchain/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -37,6 +40,61 @@ type Block struct{
 type Blockchain struct{
 	blocks []*Block
 }
+
+func createBlock(prevBlock *Block, checkoutItem BookCheckout) *Block{
+	block:= &Block{}
+	block.Pos=prevBlock.Pos+1
+	block.PrevHash=prevBlock.Hash
+	block.Timestamp=time.Now().String()
+	block.generateHash()
+
+
+	return block
+}
+
+func (block *Block) generateHash(){
+	bytes,_ := json.Marshal(block.Data)
+
+	data:= fmt.Sprint(block.Pos)+block.Hash+string(bytes)+block.Timestamp
+	
+	hash:= sha256.New()
+	hash.Write([]byte(data))
+	block.Hash= hex.EncodeToString(hash.Sum(nil))
+}
+
+func (bc *Blockchain) AddBlock(data BookCheckout){
+	prevBlock:= bc.blocks[len(bc.blocks)-1]
+
+	currBlock:= createBlock(prevBlock, data)
+
+	if validBlock(currBlock, prevBlock){
+		bc.blocks = append(bc.blocks, currBlock)
+	}
+}
+
+func validBlock(currBlock, prevBlock *Block) bool{
+	if prevBlock.Hash != currBlock.PrevHash{
+		return false
+	}
+
+	if !currBlock.validateHash(currBlock.Hash){
+		return false
+	}
+
+	if currBlock.Pos!=prevBlock.Pos+1{
+		return false
+	}
+
+	return true
+}
+
+func (block *Block) validateHash (hash string) bool{
+	block.generateHash()
+	
+	return block.Hash == hash
+}
+
+var BlockChain *Blockchain
 
 func newBook(w http.ResponseWriter, r *http.Request){
 	var book Book
@@ -69,10 +127,28 @@ func getBlockChain(w http.ResponseWriter, r *http.Request){
 }
 
 func writeBlock(w http.ResponseWriter, r *http.Request){
+	var checkoutItem BookCheckout
+	if err:= json.NewDecoder(r.Body).Decode(&checkoutItem); err!=nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Could not decode data %v",err)
+		w.Write([]byte("Could not decode checkout data"))
+		return
+	}
 
+	BlockChain.AddBlock(checkoutItem)
+}
+
+func CreateGenesisBlock() *Block{
+	return createBlock(& Block{}, BookCheckout{IsGenesis: true})
+}
+
+func NewBlockchain() *Blockchain{
+	return &Blockchain{[] *Block{CreateGenesisBlock()}}
 }
 
 func main(){
+
+	BlockChain = NewBlockchain()
 	r:=mux.NewRouter()
 	r.HandleFunc("/",getBlockChain).Methods("GET")
 	r.HandleFunc("/",writeBlock).Methods("POST")

003fc412d9d907c4ba479a2555465f4c80cab13a
Ayush Tiwari <ayushtiwari5747462@gmail.com>
2022-06-04 15:14:50 +0530

    Http handler and path parsing

--- StoryTeller/cyoa/story.go
+++ StoryTeller/cyoa/story.go
@@ -3,7 +3,16 @@ package cyoa
 import (
 	"encoding/json"
 	"io"
+	"log"
+	"net/http"
+	"text/template"
 )
+
+var tpl *template.Template
+
+func init(){
+	tpl= template.Must(template.ParseFiles("template.html"))
+}
 
 type Story map[string]Chapter
 
@@ -18,11 +27,38 @@ func JsonDecoder(r io.Reader) (Story, error){
 
 type Chapter struct{
 	Title string `json:"title"`
-	Paragraphs string `json:"story"`
+	Paragraphs []string `json:"story"`
 	Options []Option `json:"options"`
 }
 
 type Option struct{
 	Text string `json:"text"`
 	Chapter string `json:"arc"`
+}
+
+type handler struct{
+	s Story
+}
+
+func NewHandler(s Story) http.Handler{
+	return handler{s}
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	path:= r.URL.Path
+	
+	if path=="" || path=="/"{
+		path="/intro"
+	}
+	path=path[1:]
+
+	if chapter,ok:= h.s[path]; ok{
+		err:= tpl.Execute(w, chapter)
+		if err!=nil{
+		log.Fatalf("Error: %s",err)
+		http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w,"Could not find resource", http.StatusNotFound)
 }
--- StoryTeller/main.go
+++ StoryTeller/main.go
@@ -4,26 +4,34 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/ayush0x00/cyoa"
 )
 
+func handleError(msg error){
+	fmt.Printf("Error while opening the file\n")
+	log.Fatalf("Error while opening %s\n",msg)
+}
+
 func main(){
+	port:= flag.Int("port", 3000, "The port number to start server on")
 	fileName:= flag.String("file","story.json","The CYOA story in JSON format")
 	flag.Parse()
 
 	f,errFopen:= os.Open(*fileName)
 	if errFopen!=nil{
-		fmt.Printf("Error while opening the file %s\n", *fileName)
-		log.Fatalf("Error while opening %s",errFopen)
+		handleError(errFopen)
 	}
 
 	story,err:= cyoa.JsonDecoder(f)	
 	if err!=nil{
-		fmt.Println("Error while decoding the file")
-		log.Fatalf("Error while decoding: %s", err)
+		handleError(err)
 	}
 
-	fmt.Println(story)
+	h:= cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port %d\n",*port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d",*port),h))
+	
 }
